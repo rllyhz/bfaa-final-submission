4f57ba8b991a62003ce7f74570790cfabbf00b8e
rllyhz <rullyihza00@gmail.com>
2021-04-06 15:15:10 +0700

    Finishing up BottomNavigationView using Navigation Components in MainActivity.kt

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/main/MainActivity.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/main/MainActivity.kt
@@ -2,11 +2,43 @@ package id.rllyhz.dicodingsubmissionbfaa.ui.activity.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import id.rllyhz.dicodingsubmissionbfaa.R
+import id.rllyhz.dicodingsubmissionbfaa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.apply {
+            val appBarConfiguration = AppBarConfiguration(
+                    setOf(
+                            R.id.homeFragment,
+                            R.id.exploreFragment,
+                            R.id.profileFragment
+                    )
+            )
+
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            bottomNavigationView.setupWithNavController(navController)
+        }
     }
 }
