1affd4b3cc63a9103337755c974f10d02c6ea116
rllyhz <rullyihza00@gmail.com>
2021-04-12 10:56:40 +0700

    Adding Github API provider in AppModule.kt file

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/di/AppModule.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/di/AppModule.kt
@@ -1,9 +1,24 @@
 package id.rllyhz.dicodingsubmissionbfaa.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import id.rllyhz.dicodingsubmissionbfaa.api.GithubApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideGithubAPI(): GithubApi =
+        Retrofit.Builder()
+            .baseUrl(GithubApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GithubApi::class.java)
+}
