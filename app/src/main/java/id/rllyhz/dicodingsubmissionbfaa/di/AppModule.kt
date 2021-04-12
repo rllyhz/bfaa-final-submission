174dd08ac7e361fae2ef100ff41cdaa06b8ebfe9
rllyhz <rullyihza00@gmail.com>
2021-04-12 11:17:13 +0700

    Adding MainRepostory and Dispatcher providers in AppModule.kt file

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/di/AppModule.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/di/AppModule.kt
@@ -1,10 +1,15 @@
 package id.rllyhz.dicodingsubmissionbfaa.di
 
+import android.app.Application
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.rllyhz.dicodingsubmissionbfaa.api.GithubApi
+import id.rllyhz.dicodingsubmissionbfaa.ui.activity.main.MainRepository
+import id.rllyhz.dicodingsubmissionbfaa.util.DispacherProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -21,4 +26,27 @@ object AppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GithubApi::class.java)
+
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(
+        githubApi: GithubApi,
+        application: Application
+    ): MainRepository =
+        MainRepository(githubApi, application)
+
+
+    @Provides
+    @Singleton
+    fun provideDispatchers(): DispacherProvider = object : DispacherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
 }
