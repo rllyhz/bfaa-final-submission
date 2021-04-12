5ae98b3883e64428fa1a7fe96f3efc9e38f8effb
rllyhz <rullyihza00@gmail.com>
2021-04-12 11:31:54 +0700

    Adding getUsers method in MainRepository.kt file

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/main/MainRepository.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/main/MainRepository.kt
@@ -1,10 +1,30 @@
 package id.rllyhz.dicodingsubmissionbfaa.ui.activity.main
 
 import android.app.Application
+import id.rllyhz.dicodingsubmissionbfaa.R
 import id.rllyhz.dicodingsubmissionbfaa.api.GithubApi
+import id.rllyhz.dicodingsubmissionbfaa.data.model.User
+import id.rllyhz.dicodingsubmissionbfaa.util.DataConverter
+import id.rllyhz.dicodingsubmissionbfaa.util.Resource
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     private val githubApi: GithubApi,
     private val application: Application
-)
+) {
+    suspend fun getUsers(): Resource<List<User>> {
+        return try {
+            val response = githubApi.getUsers()
+            val usersResponse = response.body()
+
+            if (response.isSuccessful && usersResponse != null) {
+                val allUsers = DataConverter.usersResponseToUsersModel(usersResponse)
+                Resource.Success(allUsers)
+            } else {
+                Resource.Error(response.message())
+            }
+        } catch (e: Exception) {
+            Resource.Error(application.getString(R.string.error_message))
+        }
+    }
+}
