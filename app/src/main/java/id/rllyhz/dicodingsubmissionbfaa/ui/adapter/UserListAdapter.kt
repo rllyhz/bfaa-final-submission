4e49f0411f559872657dc2999f1f27714f914848
rllyhz <rullyihza00@gmail.com>
2021-04-12 10:42:46 +0700

    Creating UserListAdapter.kt file

---
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/adapter/UserListAdapter.kt
@@ -0,0 +1,54 @@
+package id.rllyhz.dicodingsubmissionbfaa.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import id.rllyhz.dicodingsubmissionbfaa.data.model.User
+import id.rllyhz.dicodingsubmissionbfaa.databinding.ItemUserBinding
+
+class UserListAdapter : ListAdapter<User, UserListAdapter.UserListViewHolder>(UserComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    // viewholder
+    inner class UserListViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            //
+        }
+    }
+
+
+    // diffutil for handling comparison of items
+    class UserComparator : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean =
+            oldItem == newItem // cause already using data class
+    }
+
+
+    // for item click listener
+    interface ItemClickCallback {
+        fun onDetailIconClick(user: User)
+    }
+
+    private var callback: ItemClickCallback? = null
+
+    fun setOnItemListener(listener: ItemClickCallback) {
+        callback = listener
+    }
+}
