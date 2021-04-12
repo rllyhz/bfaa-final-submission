3337eae104ed6aee1a965fd2ce42ede97fbdf16f
rllyhz <rullyihza00@gmail.com>
2021-04-12 18:22:34 +0700

    Creating User Fav UI

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/userfav/UserFavActivity.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/userfav/UserFavActivity.kt
@@ -3,10 +3,24 @@ package id.rllyhz.dicodingsubmissionbfaa.ui.activity.userfav
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import id.rllyhz.dicodingsubmissionbfaa.R
+import id.rllyhz.dicodingsubmissionbfaa.databinding.ActivityUserFavBinding
 
 class UserFavActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserFavBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_fav)
+        binding = ActivityUserFavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            title = getString(R.string.action_bar_title_user_fav)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_arrow_left)
+        }
     }
 }
