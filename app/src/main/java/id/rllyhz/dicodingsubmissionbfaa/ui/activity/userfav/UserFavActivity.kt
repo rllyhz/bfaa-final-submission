515702aa4ef72f3b3b9dd24535fa72bfa53ae8a0
rllyhz <rullyihza00@gmail.com>
2021-04-12 18:18:27 +0700

    Creating User Fav UI

---
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/userfav/UserFavActivity.kt
@@ -0,0 +1,12 @@
+package id.rllyhz.dicodingsubmissionbfaa.ui.activity.userfav
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.rllyhz.dicodingsubmissionbfaa.R
+
+class UserFavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_fav)
+    }
+}
