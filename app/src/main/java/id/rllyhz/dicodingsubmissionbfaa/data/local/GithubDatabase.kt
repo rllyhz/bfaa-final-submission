b8cdef86ef74e96d8c5ff9cc4f1eb7c2a24e4dca
rllyhz <rullyihza00@gmail.com>
2021-04-13 15:54:08 +0700

    Creating UserFav entity and UserFavDao

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/GithubDatabase.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/GithubDatabase.kt
@@ -1,4 +1,8 @@
 package id.rllyhz.dicodingsubmissionbfaa.data.local
 
-abstract class GithubDatabase {
-}
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import id.rllyhz.dicodingsubmissionbfaa.data.local.userfav.UserFav
+
+@Database(entities = [UserFav::class], version = 1)
+abstract class GithubDatabase : RoomDatabase()
--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/userfav/UserFavDao.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/userfav/UserFavDao.kt
@@ -1,5 +1,18 @@
 package id.rllyhz.dicodingsubmissionbfaa.data.local.userfav
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface UserFavDao {
+
+    @Query("SELECT * FROM user_fav_table")
     fun getAllUserFavs(): List<UserFav>
+
+    @Query("SELECT * FROM user_fav_table WHERE username = :username LIMIT 1")
+    suspend fun doesUserExist(username: String): UserFav
+
+    @Insert
+    suspend fun add(userFav: UserFav)
 }
