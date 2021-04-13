2acae2e090f1ee64bb11ac4f0142d68cd0237c2a
rllyhz <rullyihza00@gmail.com>
2021-04-13 16:01:20 +0700

    Adding UserFavDao in GithubDatabase.kt

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/GithubDatabase.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/local/GithubDatabase.kt
@@ -3,6 +3,9 @@ package id.rllyhz.dicodingsubmissionbfaa.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import id.rllyhz.dicodingsubmissionbfaa.data.local.userfav.UserFav
+import id.rllyhz.dicodingsubmissionbfaa.data.local.userfav.UserFavDao
 
 @Database(entities = [UserFav::class], version = 1)
-abstract class GithubDatabase : RoomDatabase()
+abstract class GithubDatabase : RoomDatabase() {
+    abstract fun userFavDao(): UserFavDao
+}
