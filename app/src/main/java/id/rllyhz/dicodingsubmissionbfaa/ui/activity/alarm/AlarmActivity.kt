e17287cae8f55da3886005a7cf1f677a3f369872
rllyhz <rullyihza00@gmail.com>
2021-04-13 11:47:55 +0700

    Updating Alarm UI

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/alarm/AlarmActivity.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/alarm/AlarmActivity.kt
@@ -5,9 +5,16 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import id.rllyhz.dicodingsubmissionbfaa.R
 import id.rllyhz.dicodingsubmissionbfaa.databinding.ActivityAlarmBinding
+import id.rllyhz.dicodingsubmissionbfaa.ui.feature.picker.DatePickerFragment
+import id.rllyhz.dicodingsubmissionbfaa.ui.feature.picker.TimePickerFragment
+import id.rllyhz.dicodingsubmissionbfaa.ui.receiver.AlarmReceiver
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AlarmActivity : AppCompatActivity() {
+class AlarmActivity : AppCompatActivity(), DatePickerFragment.DialogDateListener,
+    TimePickerFragment.DialogTimeListener {
     private lateinit var binding: ActivityAlarmBinding
+    private lateinit var alarmReceiver: AlarmReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,27 @@ class AlarmActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupActionBar()
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            icAlarmDate.setOnClickListener {
+                DatePickerFragment().apply {
+                    show(supportFragmentManager, DATE_PICKER_TAG)
+                }
+            }
+
+            icAlarmTime.setOnClickListener {
+                TimePickerFragment().apply {
+                    show(supportFragmentManager, TIME_PICKER_TAG)
+                }
+            }
+
+            btnAlarmSetUp.setOnClickListener {
+                //
+            }
+        }
     }
 
     private fun setupActionBar() {
@@ -34,5 +62,33 @@ class AlarmActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, date: Int) {
+        val calendar = Calendar.getInstance().apply {
+            set(year, month, date)
+        }
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        binding.tvAlarmDate.text = dateFormat.format(calendar.time)
+    }
+
+    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hourOfDay)
+            set(Calendar.MINUTE, minute)
+        }
+
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        when (tag) {
+            TIME_PICKER_TAG -> binding.tvAlarmTime.text = dateFormat.format(calendar.time)
+        }
+    }
+
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
     }
 }
