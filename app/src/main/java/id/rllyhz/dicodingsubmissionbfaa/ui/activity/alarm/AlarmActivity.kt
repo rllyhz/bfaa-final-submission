84dde6e484b2a3f1d5b9971649709afbea2fde7e
rllyhz <rullyihza00@gmail.com>
2021-04-13 11:26:39 +0700

    Updating Alarm UI

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/alarm/AlarmActivity.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/ui/activity/alarm/AlarmActivity.kt
@@ -1,12 +1,38 @@
 package id.rllyhz.dicodingsubmissionbfaa.ui.activity.alarm
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import id.rllyhz.dicodingsubmissionbfaa.R
+import id.rllyhz.dicodingsubmissionbfaa.databinding.ActivityAlarmBinding
 
 class AlarmActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlarmBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm)
+
+        binding = ActivityAlarmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            title = getString(R.string.action_bar_title_alarm_setting)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_arrow_left)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
