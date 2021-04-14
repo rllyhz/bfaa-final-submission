96612e0c11428b44895ceb3baefd873c5225a577
rllyhz <rullyihza00@gmail.com>
2021-04-14 11:22:12 +0700

    Updating Main UI

---
+++ githubconsumerapp/src/main/java/id/rllyhz/githubconsumerapp/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package id.rllyhz.githubconsumerapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.rllyhz.githubconsumerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            //
+        }
+    }
+}
