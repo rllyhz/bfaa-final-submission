cf2b6bc8a6e6447f23bc4c1c72cbd31db4f08b23
rllyhz <rullyihza00@gmail.com>
2021-04-14 10:02:45 +0700

    Creating DBGithubContentProvider.kt for content provider

---
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/data/provider/DBGithubContentProvider.kt
@@ -0,0 +1,31 @@
+package id.rllyhz.dicodingsubmissionbfaa.data.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class DBGithubContentProvider : ContentProvider() {
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int = 0
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun onCreate(): Boolean {
+        TODO("Implement this to initialize your content provider on startup.")
+    }
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        TODO("Implement this to handle query requests from clients.")
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int = 0
+}
