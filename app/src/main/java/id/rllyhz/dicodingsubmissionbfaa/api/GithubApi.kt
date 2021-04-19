770815d7c747f7823d229d1ca10f55b3e7a0d8c1
rllyhz <rullyihza00@gmail.com>
2021-04-19 16:09:44 +0700

    Creating preferences.xml file

--- app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/api/GithubApi.kt
+++ app/src/main/java/id/rllyhz/dicodingsubmissionbfaa/api/GithubApi.kt
@@ -1,33 +1,40 @@
 package id.rllyhz.dicodingsubmissionbfaa.api
 
+import id.rllyhz.dicodingsubmissionbfaa.BuildConfig
 import id.rllyhz.dicodingsubmissionbfaa.data.response.SearchUsersResponse
 import id.rllyhz.dicodingsubmissionbfaa.data.response.UserDetailResponse
 import id.rllyhz.dicodingsubmissionbfaa.data.response.UsersResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubApi {
     @GET("users")
+    @Headers("Authorization: token $GITHUB_API_TOKEN")
     suspend fun getUsers(): Response<List<UsersResponse>>
 
     @GET("users/{username}")
+    @Headers("Authorization: token $GITHUB_API_TOKEN")
     suspend fun getUserDetailOf(
         @Path("username") username: String
     ): Response<UserDetailResponse>
 
     @GET("users/{username}/following")
+    @Headers("Authorization: token $GITHUB_API_TOKEN")
     suspend fun getFollowingOfUser(
         @Path("username") username: String
     ): Response<List<UsersResponse>>
 
     @GET("users/{username}/followers")
+    @Headers("Authorization: token $GITHUB_API_TOKEN")
     suspend fun getFollowersOfUser(
         @Path("username") username: String
     ): Response<List<UsersResponse>>
 
     @GET("search/users")
+    @Headers("Authorization: token $GITHUB_API_TOKEN")
     suspend fun searchUsers(
         @Query("q") query: String
     ): Response<SearchUsersResponse>
@@ -35,5 +42,6 @@ interface GithubApi {
 
     companion object {
         const val BASE_URL = "https://api.github.com/"
+        private const val GITHUB_API_TOKEN = BuildConfig.GITHUB_API_TOKEN
     }
 }
